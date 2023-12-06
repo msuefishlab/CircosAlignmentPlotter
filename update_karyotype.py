2160e5a0aaefc67be375661093d5d07b560cca0c
Jason Gallant <jgallant@msu.edu>
2023-12-06 16:56:53 -0500

    updated with chromosome ordering or size ordering

--- update_karyotype.py
+++ update_karyotype.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 
 def read_table(file_path):
     with open(file_path, 'r') as file:
@@ -13,83 +13,79 @@ def parse_chr_details(lines):
             mapping[parts[2]] = (f"{species_formatted}_chr{parts[0]}", parts[1])
     return mapping
 
-def replace_identifiers(karyotype_lines, chr_details):
+def process_karyotype(karyotype_lines, chr_details):
     updated_lines = []
-    species_data = {}
-    av_values = []
     av_chr_pairs = []
+    species_data = {}
 
     for line in karyotype_lines:
         parts = line.split()
-        identifier = parts[3]
-        av_value = parts[2]  # Extracting the avXX value
-        av_values.append(av_value)
+        identifier, av_value = parts[3], parts[2]
         if identifier in chr_details:
             new_identifier, species = chr_details[identifier]
-            parts[3] = new_identifier
             chr_number = new_identifier.split('_')[-1]
-            if chr_number=="chr24":
-                chr_number="chrx"
-            elif chr_number=="chr25":
-                chr_number="chry"
-            parts[-1] = chr_number
+            original_chr_number = chr_number.replace("chr","")
+            chr_number = "chrx" if chr_number == "chr24" else "chry" if chr_number == "chr25" else chr_number
+            parts[3], parts[-1] = new_identifier, chr_number
             updated_line = ' '.join(parts)
-            av_chr_pairs.append((av_value, chr_number))  # Store av-chr pair
-            if species not in species_data:
-                species_data[species] = []
-            species_data[species].append((updated_line, int(parts[4])))
+            size = int(parts[5])
+            av_chr_pairs.append((av_value, chr_number, species, size,original_chr_number))
+            species_data.setdefault(species, []).append((updated_line, size))
         else:
             updated_lines.append(line)
-    return updated_lines, species_data, av_values,av_chr_pairs
 
-def sort_species_data(species_data):
-    sorted_data = []
+    return updated_lines, species_data, av_chr_pairs
+
+def sort_data(species_data, av_chr_pairs,sort_by_chr_number):
+    sorted_karyotype = []
     for species, data in species_data.items():
         data.sort(key=lambda x: x[1], reverse=species == 'bb')
-        sorted_data.extend([line for line, _ in data])
-    return sorted_data
+        sorted_karyotype.extend([line for line, _ in data])
+    
+    # Corrected sorting logic for av_chr_pairs
+    if sort_by_chr_number:
+         av_chr_pairs.sort(key=lambda x: (x[2], int(x[4]) if x[2] =='bb' else -int(x[4])))
+    else:
+        av_chr_pairs.sort(key=lambda x: (x[2], -x[3] if x[2] == 'bb' else x[3]))
+    return sorted_karyotype, av_chr_pairs
+
 
 def write_to_file(lines, file_path):
     with open(file_path, 'w') as file:
         for line in lines:
             file.write(line + '\n')
 
-def main(karyotype_file, chr_details_file, output_file):
-    # Read the tables
-    karyotype_lines = read_table(karyotype_file)
-    chr_details_lines = read_table(chr_details_file)
+def print_output(av_chr_pairs):
+    print("Please paste the following into chromosome order in your circos.conf")
+    sorted_av_values = [av for av, _, _, _ ,_ in av_chr_pairs]
+    print(','.join(sorted_av_values))
 
-    # Parse the chr_details table
-    chr_details = parse_chr_details(chr_details_lines)
-
-    # Replace identifiers in karyotype and separate by species
-    updated_karyotype, species_data, av_values,av_chr_pairs = replace_identifiers(karyotype_lines, chr_details)
-
-    # Sort data for each species
-    sorted_karyotype = sort_species_data(species_data)
-
-    # Combine sorted data with unclassified lines
-    final_output = sorted_karyotype + updated_karyotype
-
-    # Write the result to the output file
-    write_to_file(final_output, output_file)
-    
-	# Print the avXX values as a comma-separated list
-    print("Please paste the following into chromsome order in your circos.conf")
-    print(','.join(av_values))
-
-    #Print the color configuration:
-    # Print av-chr pairs
     print("Please paste the following into the root level of your circos.conf")
     print("<colors>")
-    for av, chr_num in av_chr_pairs:
-        print(f"{av} = {chr_num}")
+    for av_value, chr_number, _, _,_ in av_chr_pairs:
+        print(f"{av_value} = {chr_number}")
     print("</colors>")
 
+def main():
+    parser = argparse.ArgumentParser(description='Process karyotype and chromosome details.')
+    parser.add_argument('karyotype_file', type=str, help='Path to karyotype file')
+    parser.add_argument('chr_details_file', type=str, help='Path to chromosome details file')
+    parser.add_argument('output_file', type=str, help='Path to output file')
+    parser.add_argument('--sort_by_chr_number', action='store_true', help='Sort by chromosome number')
+
+    args = parser.parse_args()
+
+    karyotype_lines = read_table(args.karyotype_file)
+    chr_details_lines = read_table(args.chr_details_file)
+    chr_details = parse_chr_details(chr_details_lines)
+
+    updated_karyotype, species_data, av_chr_pairs = process_karyotype(karyotype_lines, chr_details)
+    sorted_karyotype, sorted_av_chr_pairs = sort_data(species_data, av_chr_pairs, args.sort_by_chr_number)
+
+    final_output = sorted_karyotype + updated_karyotype
+    write_to_file(final_output, args.output_file)
+    print_output(sorted_av_chr_pairs)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py karyotype.txt chr_details.txt output.txt")
-        sys.exit(1)
+    main()
 
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
